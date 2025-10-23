1a37012e21bdfe0bfee1e2126158dfc119b8a975
Fadi NADER <fadinader@MacBook-Pro-de-Fadi.local>
2025-10-23 12:34:35 +0200

    fix: réactivation de l’intent GuildMembers + doc test

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers
   ]
 });
 
